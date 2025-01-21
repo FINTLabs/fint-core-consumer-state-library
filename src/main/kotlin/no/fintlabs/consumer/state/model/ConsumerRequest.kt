4062a90daf43bc96dac2962664ee4f045b600431
Hknots <hknots99@gmail.com>
2025-01-21 14:49:58 +0100

    create ConsumerRequest

--- src/main/kotlin/no/fintlabs/consumer/state/model/ConsumerRequest.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/model/ConsumerRequest.kt
@@ -1,6 +1,16 @@
 package no.fintlabs.consumer.state.model
 
+import no.fintlabs.consumer.state.interfaces.ConsumerFields
+import no.fintlabs.consumer.state.interfaces.ConsumerIdentificator
+
 data class ConsumerRequest(
-    val consumer: Consumer,
-    val operation: Operation
-)
+    override val domain: String,
+    override val `package`: String,
+    override val managed: Boolean,
+    override val version: String,
+    override val org: String,
+    override val podResources: PodResources,
+    override val resources: List<String>,
+    override val writeableResources: List<String>,
+    override val cacheDisabledResources: List<String>,
+) : ConsumerIdentificator, ConsumerFields
