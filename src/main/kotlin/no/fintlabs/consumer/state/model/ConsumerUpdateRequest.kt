5177d90fa421dd01c06c9f9d773781411615e9b6
Hknots <hknots99@gmail.com>
2025-01-22 08:30:49 +0100

    change fields to nullable

--- src/main/kotlin/no/fintlabs/consumer/state/model/ConsumerUpdateRequest.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/model/ConsumerUpdateRequest.kt
@@ -3,10 +3,10 @@ package no.fintlabs.consumer.state.model
 import no.fintlabs.consumer.state.interfaces.ConsumerFields
 
 data class ConsumerUpdateRequest(
-    override val version: String,
-    override val managed: Boolean,
-    override val resources: List<String>,
-    override val podResources: PodResources,
-    override val writeableResources: List<String>,
-    override val cacheDisabledResources: List<String>
+    override val version: String?,
+    override val managed: Boolean?,
+    override val resources: List<String>?,
+    override val podResources: PodResources?,
+    override val writeableResources: List<String>?,
+    override val cacheDisabledResources: List<String>?
 ) : ConsumerFields
