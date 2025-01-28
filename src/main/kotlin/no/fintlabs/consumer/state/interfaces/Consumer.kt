6f5d813a75580451d8588b03f4c4c30e35651b6c
Hknots <hknots99@gmail.com>
2025-01-28 11:50:17 +0100

    feat: fields are no longer nullable

--- src/main/kotlin/no/fintlabs/consumer/state/interfaces/Consumer.kt
+++ src/main/kotlin/no/fintlabs/consumer/state/interfaces/Consumer.kt
@@ -1,4 +1,15 @@
 package no.fintlabs.consumer.state.interfaces
 
+import no.fintlabs.consumer.state.model.PodResources
+
 interface Consumer: ConsumerIdentificator, ConsumerFields {
+    override val domain: String
+    override val `package`: String
+    override val org: String
+    override val shared: Boolean
+    override val version: String
+    override val resources: List<String>
+    override val podResources: PodResources
+    override val writeableResources: List<String>
+    override val cacheDisabledResources: List<String>
 }
